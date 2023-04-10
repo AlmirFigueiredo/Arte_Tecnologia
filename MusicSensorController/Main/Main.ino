196a30d46909fe765fff7b60a78e0d63b7bfcfd2
Jose Almir <joseamfj@outlook.com>
2023-04-10 16:12:54 -0300

    Create, setup and calculate distances of ultrasonic sensors

--- MusicSensorController/Main/Main.ino
+++ MusicSensorController/Main/Main.ino
@@ -1,13 +1,28 @@
 const int trigPinUS1 = 1;
 const int echoPinUS1 = 2;
 
+const int trigPinUS2 = 3;
+const int echoPinUS2 = 4;
+
+const int trigPinUS3 = 5;
+const int trigPinUS3 = 6;
+
+const int trigPinUS4 = 7;
+const int trigPinUS4 = 8;
+
 long duration;
-int distance;
+int distanceUS1, distanceUS2, distanceUS3, distanceUS4;
 
 void setup() {
     Serial.begin(9600);
-    pinMode(trigPin, OUTPUT);
-    pinMode(echoPin, INPUT);
+    pinMode(trigPinUS1, OUTPUT);
+    pinMode(echoPinUS1, INPUT);
+    pinMode(trigPinUS2, OUTPUT);
+    pinMode(echoPinUS2, INPUT);
+    pinMode(trigPinUS3, OUTPUT);
+    pinMode(echoPinUS3, INPUT);
+    pinMode(trigPinUS4, OUTPUT);
+    pinMode(echoPinUS4, INPUT);
 }
 int calculateDistance(int trigPin, int echoPin) {
     digitalWrite(trigPin, LOW);
@@ -20,8 +35,11 @@ int calculateDistance(int trigPin, int echoPin) {
 }
 void loop() {
     int distanceUS1 = calculateDistance(trigPinUS1, echoPinUS1);
-    
+    int distanceUS2 = calculateDistance(trigPinUS2, echoPinUS2);
+    int distanceUS3 = calculateDistance(trigPinUS3, echoPinUS3);
+    int distanceUS4 = calculateDistance(trigPinUS4, echoPinUS4);
 
-    Serial.println(distance);
+    
+    Serial.println(distanceUS1);
     delay(1000);
 }
