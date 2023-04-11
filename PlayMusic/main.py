73cc4fe4894418ef59b4d4e33351806847fa2cb0
Jose Almir <joseamfj@outlook.com>
2023-04-10 21:04:42 -0300

    Get distances from arduino

--- PlayMusic/main.py
+++ PlayMusic/main.py
@@ -1,1 +1,15 @@
+import serial
 
+s = serial.Serial("dev/ttyACM0", 9600)
+
+while True:
+    distances_string = s.redline().decode().strip()
+    distances = distances_string.split()
+
+    distanceUS1 = int(distances[0])
+    distanceUS2 = int(distances[1])
+    distanceUS3 = int(distances[2])
+    distanceUS4 = int(distances[3])
+    distanceUS5 = int(distances[4])
+    
+    
