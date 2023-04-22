ddfadc2c6d72e186879c5b9003a8cad7f1325d4b
Jose Almir <joseamfj@outlook.com>
2023-04-22 20:17:16 -0300

    Setup pins

--- MusicSensorController/Main/Main.ino
+++ MusicSensorController/Main/Main.ino
@@ -2,4 +2,11 @@ const int triggerPins[6] = {2, 3, 4, 5, 6, 7};
 const int echoPins[6] = {8, 9, 10, 11, 12, 13};
 float distances[6];
 
+void setup() {
+    Serial.begin(9600);
+    for(int i = 0; i < 6; i++) {
+        pinMode(triggerPins[i], OUTPUT);
+        pinMode(echoPins[i], INPUT);
+    }
+}
 
