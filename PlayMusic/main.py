b0314088512215d48be0c294cfdd0ea797eacd20
Jose Almir <joseamfj@outlook.com>
2023-04-22 20:53:24 -0300

    Import libraries and create var to determinate the port

--- PlayMusic/main.py
+++ PlayMusic/main.py
@@ -1,15 +1,9 @@
 import serial
+import random
+from pygame import mixer
+from time import sleep
 
 s = serial.Serial("dev/ttyACM0", 9600)
 
-while True:
-    distances_string = s.redline().decode().strip()
-    distances = distances_string.split()
 
-    distanceUS1 = int(distances[0])
-    distanceUS2 = int(distances[1])
-    distanceUS3 = int(distances[2])
-    distanceUS4 = int(distances[3])
-    distanceUS5 = int(distances[4])
-    
     
