ecf302ce7c5f2bb115750f388aaa2f53e775f48c
Jose Almir <joseamfj@outlook.com>
2023-04-22 21:19:08 -0300

    Create initial var

--- PlayMusic/main.py
+++ PlayMusic/main.py
@@ -9,4 +9,8 @@ mixer.init()
 group1 = [mixer.Sound(f"Song{i}.wav") for i in range(1, 9)]
 group2 = [mixer.Sound(f"Song{i}.wav") for i in range(10, 17)]
 contemplativ_song = mixer.Sound("SoundGroups/Contemplativa.mp3") 
+played_songs = []
+last_played_song = None
+contemplativ_song_playing = False
+
 
