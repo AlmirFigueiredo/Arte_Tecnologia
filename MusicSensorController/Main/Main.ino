3fab5914ae51eefd28b088dc790945f946f6b5be
GabLunaDev <gabriel.00000844590@unicap.br>
2023-05-03 16:08:52 -0300

    Modifing to prototype

--- MusicSensorController/Main/Main.ino
+++ MusicSensorController/Main/Main.ino
@@ -1,10 +1,10 @@
-const int triggerPins[6] = {2, 3, 4, 5, 6, 7};
-const int echoPins[6] = {8, 9, 10, 11, 12, 13};
+const int triggerPins[4] = {2, 4, 6};
+const int echoPins[4] = {3, 5, 7};
 float tempDistance;
 
 void setup() {
     Serial.begin(9600);
-    for(int i = 0; i < 6; i++) {
+    for(int i = 0; i < 4; i++) {
         pinMode(triggerPins[i], OUTPUT);
         pinMode(echoPins[i], INPUT);
     }
@@ -17,19 +17,18 @@ float getDistance(int triggerPin, int echoPin) {
     delayMicroseconds(10);
     digitalWrite(triggerPin, LOW);
     float duration = pulseIn(echoPin, HIGH);
-    return (duration(*0.034/2));
+    return (duration*(0.034/2));
 }
 
 void loop() {
-    for(int i = 0; i < 6; i++) {
+    for(int i = 0; i < 3; i++) {
         tempDistance = getDistance(triggerPins[i], echoPins[i]);
-        if(i < 5) {
-            Serial.print(distance, 2);
+        if(i < 2) {
+            Serial.print(tempDistance, 2);
             Serial.print(",");
         } else {
-            Serial.println(distance, 2);
+            Serial.println(tempDistance, 2);
         }
     }
-    delay(1000);
+    delay(500);
 }
-
--- PlayMusic/main.py
+++ PlayMusic/main.py
@@ -6,7 +6,7 @@ from time import sleep
 s = serial.Serial("dev/ttyACM0", 9600)
 mixer.init()
 
-MAX_DISTANCE = 200
+MAX_DISTANCE = 70
 MIN_DISTANCE = 10
 group1 = [mixer.Sound(f"Song{i}.wav") for i in range(1, 9)]
 group2 = [mixer.Sound(f"Song{i}.wav") for i in range(9, 17)]
@@ -45,9 +45,9 @@ while True:
     imported_data = s.readline().decode("utf-8").strip()
     distances = [float(x) for x in imported_data.split(",")]
     
-    is_in_middle = ((distances[2] > MIN_DISTANCE and distances[2] < MAX_DISTANCE)or(distances[3] > MIN_DISTANCE and distances[3] < MAX_DISTANCE))
-    is_in_left = ((distances[0] > MIN_DISTANCE and distances[0] < MAX_DISTANCE)or(distances[1] > MIN_DISTANCE and distances[1] < MAX_DISTANCE))
-    is_in_right = ((distances[4] > MIN_DISTANCE and distances[4] < MAX_DISTANCE) or (distances[5] > MIN_DISTANCE and distances[5] < MAX_DISTANCE))
+    is_in_middle = ((distances[2] > MIN_DISTANCE and distances[2] < MAX_DISTANCE))
+    is_in_left = ((distances[0] > MIN_DISTANCE and distances[0] < MAX_DISTANCE))
+    is_in_right = ((distances[1] > MIN_DISTANCE and distances[1] < MAX_DISTANCE))
 
     visitors_left = visitors_left + 1 if is_in_left else max(0, visitors_left - 1) # Pois o menor valor possivel e 0
     visitors_middle = visitors_middle + 1 if is_in_middle else max(0, visitors_middle - 1)
