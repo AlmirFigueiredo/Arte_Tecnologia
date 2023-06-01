ddd75a820a24a70125d1c8fde88bb78025bcc214
Jose Almir <joseamfj@outlook.com>
2023-06-01 14:53:44 -0300

    Fix bugs in Arduino Uno code

--- MusicSensorController/Main/Main.ino
+++ MusicSensorController/Main/Main.ino
@@ -17,19 +17,18 @@ float getDistance(int triggerPin, int echoPin) {
     delayMicroseconds(10);
     digitalWrite(triggerPin, LOW);
     float duration = pulseIn(echoPin, HIGH);
-    return (duration(*0.034/2));
+    return (duration*(0.034/2));
 }
 
 void loop() {
     for(int i = 0; i < 6; i++) {
         tempDistance = getDistance(triggerPins[i], echoPins[i]);
         if(i < 5) {
-            Serial.print(distance, 2);
+            Serial.print(tempDistance, 2);
             Serial.print(",");
         } else {
-            Serial.println(distance, 2);
+            Serial.println(tempDistance, 2);
         }
     }
     delay(1000);
 }
-
