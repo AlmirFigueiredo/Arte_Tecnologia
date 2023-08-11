c96b408edd3b3bdd366a2daece9121651a911df3
Jose Almir <joseamfj@outlook.com>
2023-08-10 21:19:57 -0300

    Create arduino source code

--- source.ino
+++ source.ino
@@ -1,5 +1,3 @@
-
-
 #define ECHO_A 2
 #define ECHO_B 3
 #define ECHO_C 4
@@ -7,10 +5,6 @@
 #define TRIG_A 5
 #define TRIG_B 6
 #define TRIG_C 7
-
-#define LED_A 8
-#define LED_B 9
-#define LED_C 10
 
 void setup() {
     pinMode(ECHO_A, INPUT);
@@ -21,9 +15,35 @@ void setup() {
     pinMode(TRIG_B, OUTPUT);
     pinMode(TRIG_C, OUTPUT);
 
-    pinMode(LED_A, OUTPUT);
-    pinMode(LED_B, OUTPUT);
-    pinMode(LED_C, OUTPUT);
-
     Serial.begin(9600);
+}
+
+float measureDistance(int trigPin, int echoPin) {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    
+    long duration = pulseIn(echoPin, HIGH);
+    
+    float distance = (duration * 0.0343) / 2;
+    
+    return distance;
+}
+
+void loop() {
+    float distanceA = measureDistance(TRIG_A, ECHO_A);
+    float distanceB = measureDistance(TRIG_B, ECHO_B);
+    float distanceC = measureDistance(TRIG_C, ECHO_C);
+
+    // Envia as distâncias separadas por vírgulas e termina com uma nova linha
+    Serial.print(distanceA);
+    Serial.print(",");
+    Serial.print(distanceB);
+    Serial.print(",");
+    Serial.println(distanceC);
+
+    delay(1000); 
 }
